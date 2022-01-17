0f81318c5ae56b5d4b1decccc8d20830cb6ee16f
Maulana Sodiqin <maulana@pmg.id>
2022-01-17 21:37:23 +0700

    Maulana Sodiqin | Add Config

--- assets/js/component/index.js
+++ assets/js/component/index.js
@@ -388,13 +388,13 @@ export const TableKeyboard = {
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         Nama Keyboard
       </label>
-      <input v-model="keebName" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+      <input v-model="keebName" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Masukkan Nama Keyboard">
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Brand Keyboard
       </label>
-      <input v-model="keebBrand" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <input v-model="keebBrand" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Masukkan Brand Keyboard">
     </div>
   </div>
   <div class="flex flex-wrap -mx-3 mb-6 space-y-6">
