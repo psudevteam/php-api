dd1a751d6ac6f4b3f4623b0d6e61d2d147fd57d2
Maulana Sodiqin <maulana@pmg.id>
2022-01-17 20:30:45 +0700

    Maulana Sodiqin | Add Component JS

--- api/keyboard/post.php
+++ api/keyboard/post.php
@@ -2,7 +2,7 @@
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: data');
+  header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
   include_once '../../config/index.php';
